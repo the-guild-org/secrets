c09d675f110f0e8632de91f7acd791c593e51172
enisdenjo <badurinadenis@gmail.com>
2022-10-03 15:44:05 +0200

    bundle in proj root

--- build.mjs
+++ build.mjs
@@ -6,7 +6,7 @@ build({
   minify: true,
   platform: 'node',
   format: 'esm',
-  outfile: 'dist/index.mjs',
+  outfile: 'bundle.mjs',
   banner: {
     // https://github.com/evanw/esbuild/issues/1921#issuecomment-1152991694
     js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
